e1b08fb43e44beb422b215de2978a73b4cf99a3f
Muhammad Salihu Belel <salihubelel406@gmail.com>
2025-07-31 18:22:20 +0100

    Updated JoinAsSeller page and UI sectionsmodified:   package-lock.json     renamed:    src/app/page.jsx -> src/app/Seller Privacy Policy/page.jsx     modified:   src/app/join-as-seller/page.jsx     modified:   src/app/layout.jsx     modified:   text

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${montserrat.variable}`}>
       <body
-        className="bg-[#f8f9fa]"
+        className=" dark bg-[#f8f9fa]"
       > < Navbar />
         {children}
         
